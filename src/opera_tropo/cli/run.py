a63283d2828fed27fa86ce7c79341d75bc509d3b
mgovorcin <mgovorcinzd@gmail.com>
2025-01-29 16:41:31 -0800

    adding cli

---
+++ src/opera_tropo/cli/run.py
@@ -0,0 +1,26 @@
+###### DEVELOPEMENT STAGE ############
+import click
+
+__all__ = ["run_cli", "run_main"]
+
+
+def run_main(config_file: str, debug: bool = False) -> None:
+    """Run the displacement workflow for CONFIG_FILE."""
+    # rest of imports here so --help doesn't take forever
+    from src.opera_tropo.main import run
+    from opera_tropo.config.pge_runconfig import RunConfig
+
+    pge_runconfig = RunConfig.from_yaml(config_file)
+    cfg = pge_runconfig.to_workflow()
+    run(cfg, pge_runconfig=pge_runconfig, debug=debug)
+
+
+@click.command("run")
+@click.argument("config_file", type=click.Path(exists=True))
+@click.pass_context
+def run_cli(
+    ctx: click.Context,
+    config_file: str,
+) -> None:
+    """Run the troposphere correction workflow for CONFIG_FILE."""
+    run_main(config_file=config_file, debug=ctx.obj["debug"])
