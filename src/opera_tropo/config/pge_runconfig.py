b0954b5ca7a0bb6850b9173f24a0574b6cffdbfa
mgovorcin <mgovorcinzd@gmail.com>
2025-01-29 16:39:03 -0800

    configuration module

---
+++ src/opera_tropo/config/pge_runconfig.py
@@ -0,0 +1,133 @@
+###### DEVELOPEMENT STAGE ############
+from pathlib import Path
+from typing import Any, ClassVar, List, Optional, Union
+from pydantic import ConfigDict, Field
+
+from ._yaml import YamlModel
+from .runconfig import  (WorkerSettings, 
+                         InputOptions, 
+                         OutputOptions, 
+                         TropoWorkflow)
+
+'''
+# Note add options here to download file
+class InputFileGroup(YamlModel):
+    """Inputs for A group of input files."""
+
+    input_file_path: str = Field(
+        default_factory=str,
+        description="Path to HRES model file.",
+    )
+
+    model_date: str = Field(
+        default_factory=str,
+        description="HRES model date. eg 20190101.",
+    )
+
+    model_hour: str = Field(
+        default_factory=str,
+        description="HRES model hour. eg 06",
+    )
+
+    s3_bucket: str = Field(
+        default_factory=str,
+        description="HRES model s3 bucket.",
+    )
+
+    model_config = ConfigDict(extra="forbid")
+'''
+class PrimaryExecutable(YamlModel):
+    """Group describing the primary executable."""
+
+    product_type: str = Field(
+        default="OPERA_TROPO",
+        description="Product type of the PGE.",
+    )
+    model_config = ConfigDict(extra="forbid")
+
+
+class ProductPathGroup(YamlModel):
+    """Group describing the product paths."""
+
+    product_path: Path = Field(
+        default=Path("."),
+        description="Directory where PGE will place results",
+    )
+    scratch_path: Path = Field(
+        default=Path("./scratch"),
+        description="Path to the scratch directory.",
+    )
+    output_directory: Path = Field(
+        default=Path("./output"),
+        description="Path to the SAS output directory.",
+        # The alias means that in the YAML file, the key will be "sas_output_path"
+        # instead of "output_directory", but the python instance attribute is
+        # "output_directory" (to match DisplacementWorkflow)
+        alias="sas_output_path",
+    )
+    product_version: str = Field(
+        default="0.1",
+        description="Version of the product, in <major>.<minor> format.",
+    )
+    model_config = ConfigDict(extra="forbid")
+
+
+class RunConfig(YamlModel):
+    """A PGE run configuration."""
+
+    # Used for the top-level key
+    name: ClassVar[str] = "opera_tropo_workflow"
+
+    input_file: InputOptions = Field(default_factory=InputOptions)
+    output_options: OutputOptions = Field(default_factory=OutputOptions)
+    primary_executable: PrimaryExecutable = Field(default_factory=PrimaryExecutable)
+    product_path_group: ProductPathGroup = Field(default_factory=ProductPathGroup)
+
+    # General workflow metadata
+    worker_settings: WorkerSettings = Field(default_factory=WorkerSettings)
+
+    log_file: Optional[Path] = Field(
+        default=Path("output/opera_tropo_workflow.log"),
+        description="Path to the output log file in addition to logging to stderr.",
+    )
+    #model_config = ConfigDict(extra="forbid")
+
+    @classmethod
+    def model_construct(cls, **kwargs):
+        """Recursively use model_construct without validation."""
+        if "input_file" not in kwargs:
+            kwargs["input_file"] = InputOptions.model_construct()
+        if "product_path_group" not in kwargs:
+            kwargs["product_path_group"] = ProductPathGroup.model_construct()
+        return super().model_construct(
+            **kwargs,
+        )
+
+    def to_workflow(self):
+        """Convert to a `TropoWorkflow` object."""
+        # We need to go from the PGE format to opera_tropo's TropoWorkflow:
+        # Note that the top two levels of nesting can be accomplished by wrapping
+        # the normal model export in a dict.
+        #
+        # The things from the RunConfig that are used in the
+        # TropoWorkflow are the input files,
+        # the output directory, and the scratch directory.
+
+        #input_file = (self.input_file_group.input_file_group.input_file_path
+        scratch_directory = self.product_path_group.scratch_path
+        tmp_directory = Path(scratch_directory) / 'tmp'
+        tmp_directory.mkdir(parents=True, exist_ok=True)
+        worker_settings = self.worker_settings.copy()
+        worker_settings.dask_temp_dir = str(tmp_directory)
+        output_options = self.output_options.copy()
+        output_options.product_version = self.product_path_group.product_version
+
+        return TropoWorkflow(
+            input_options=self.input_file.__dict__,
+            output_options=output_options.__dict__,
+            work_directory=scratch_directory,
+            # These ones directly translate
+            worker_settings=worker_settings.__dict__,
+            log_file=self.log_file,
+            )
+    
---
+++ src/opera_tropo/config/runconfig.py
@@ -0,0 +1,205 @@
+from __future__ import annotations
+
+import logging
+from datetime import datetime, timezone
+from pathlib import Path
+from typing import Any, Optional, Dict
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    PrivateAttr,
+)
+
+from ._yaml import YamlModel
+
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    "InputOptions",
+    "OutputOptions",
+    "WorkerSettings",
+    "TropoWorkflow",
+]
+
+PRODUCT_VERSION = '0.1'
+DEFAULT_ENCODING_OPTIONS = {
+        "compression_flag": False, # run
+        "zlib": True,
+        "complevel": 4,
+        "shuffle": True
+        }
+
+
+# Base model
+## NOTE add option to specify s3 path
+'''
+import s3fs
+fs = s3fs.S3FileSystem(anon=True)
+aws_url = 's3://opera-dev-lts-fwd-hyunlee/20190825/D08250000082500001.zz.nc'
+'''
+
+class InputOptions(BaseModel, extra="forbid"):
+    """Options specifying input datasets for workflow."""
+
+    _directory: Path = PrivateAttr(Path("data"))
+
+    input_file_path: str = Field(
+        default_factory=str,
+        description="Path to the input HRES model hres_model.nc",
+        )
+
+    date_fmt: str = Field(
+        "%Y%m%d",
+        description="Format of dates contained in s3 HRES folder",
+    )
+
+    #@root_validator(pre=True)
+    #def check_input_file_path(cls, values):
+    #    """Validator to ensure that input_file_path is specified."""
+    #    input_file_path = values.get('input_file_path', None)
+    #    if not input_file_path or input_file_path.strip() == "":
+    #        raise ValueError("input_file_path must be specified in the configuration file.")
+    #    return values
+
+class OutputOptions(BaseModel, extra="forbid"):
+    """Options specifying input datasets for workflow."""
+
+    _directory: Path = PrivateAttr(Path("output"))
+
+    date_fmt: str = Field(
+        "%Y%m%dT%H%M%S",
+        description="Output Date Format for OPERA TROPO",
+    )
+
+    creation_time: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        description="Time the config file was created",
+    )
+
+    output_heights: Optional[list] = Field(
+        default_factory=list,
+        description=("Output height level to hydrostatic and wet delay"
+                     " default:  RAiDER HRES 145 height levels."),
+    )
+    compression_kwargs: Optional[Dict[str, Any]] = Field(
+        default_factory=lambda: DEFAULT_ENCODING_OPTIONS,
+    description="Product output compression options for netcdf", 
+    )
+
+    product_version : str = Field(
+       PRODUCT_VERSION,
+       description="OPERA TROPO product version", 
+    )
+
+    model_config = ConfigDict(extra="forbid", validate_default=True)
+
+    def get_output_filename(self, date:str, hour:str | int):
+        """
+        Get product output filename convention
+        Product level spec: https://www.earthdata.nasa.gov/learn/earth-observation-data-basics/data-processing-levels
+        """
+
+        date_time = datetime.strptime(f"{date}T{hour}", '%Y%m%dT%H')
+        date_time = date_time.strftime(self.date_fmt)
+        proc_datetime = self.creation_time.strftime(self.date_fmt) 
+        return f"OPERA_L4_TROPO_GLOBAL_{date_time}Z_{proc_datetime}Z_HRES_0.1_v{self.product_version}.nc"
+
+class WorkerSettings(BaseModel, extra="forbid"):
+    """Settings for controlling CPU settings and parallelism."""
+    n_workers: int = Field(
+        1,
+        ge=1,
+        description=(
+            "Number of workers to use in dask.Client."
+        ), 
+    )
+    threads_per_worker: int = Field(
+        1,
+        ge=1,
+        description=(
+            "Number of threads to use per worker in dask.Client"
+        ),
+    )
+    max_memory: int = Field(
+        default=4,
+        ge=4,
+        description=(
+            "Workers are given a target memory limit in dask.Client"
+        ),
+    )
+    dask_temp_dir: str | Path = Field(
+        None,
+        description=(
+            "Dask local spill directory."
+        ),
+    )
+    block_shape: tuple[int, int] = Field(
+        (128, 128),
+        description="Size (rows, columns) of blocks of data to load at a time.",
+    )
+
+
+class WorkflowBase(YamlModel):
+    """Base of multiple workflow configuration models."""
+
+    # Paths to input/output files
+    input_options: InputOptions = Field(default_factory=InputOptions)
+    output_options: OutputOptions = Field(default_factory=OutputOptions)
+
+    work_directory: Path = Field(
+        Path(),
+        description="Name of directory to use for writing output files",
+        validate_default=True,
+    )
+    keep_paths_relative: bool = Field(
+        False,
+        description=(
+            "Don't resolve filepaths that are given as relative to be absolute."
+        ),
+    )
+
+    # General workflow metadata
+    worker_settings: WorkerSettings = Field(default_factory=WorkerSettings)
+    log_file: Optional[Path] = Field(
+        default=None,
+        description=(
+            "Path to output log file (in addition to logging to `stderr`)."
+            " Default logs to `tropo.log` within `work_directory`"
+        ),
+    )
+
+    model_config = ConfigDict(extra="allow")
+    #_tropo_version: str = PrivateAttr(_tropo_version)
+    # internal helpers
+    # Stores the list of directories to be created by the workflow
+    _directory_list: list[Path] = PrivateAttr(default_factory=list)
+
+    def model_post_init(self, context: Any, /) -> None:
+        """After validation, set up properties for use during workflow run."""
+        super().model_post_init(context)
+        # Ensure outputs from workflow steps are within work directory.
+        if not self.keep_paths_relative:
+            # Save all directories as absolute paths
+            self.work_directory = self.work_directory.resolve(strict=False)
+
+    def create_dir_tree(self) -> None:
+        """Create the directory tree for the workflow."""
+        for d in self._directory_list:
+            logger.debug(f"Creating directory: {d}")
+            d.mkdir(parents=True, exist_ok=True)
+
+##### WORKFLOW #######
+# NOTE: add functions associated with the tropo_workflow
+class TropoWorkflow(WorkflowBase, extra="forbid"):
+    """Configuration for the troposphere delay calculation"""
+
+    _tropo_directory: Path = Path("tropo")
+    #_tmp_directory: Path = Path("tmp")
+
+    # Paths to input/output files
+    input_options: InputOptions = Field(default_factory=InputOptions)
+  
+    output_options: OutputOptions = Field(default_factory=OutputOptions)
+
